f7bcb5298ae5c608c2d1d3e128845d92e140f754
Stephen-Zulauf <szulauf1990@gmail.com>
2025-05-15 21:12:58 -0700

    finished routes up to deleting

--- assignment7/users_controller.mjs
+++ assignment7/users_controller.mjs
@@ -1,27 +1,119 @@
-import 'dotenv/config';
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import * as users from './users_model.mjs';
+import "dotenv/config";
+import express from "express";
+import asyncHandler from "express-async-handler";
+import * as users from "./users_model.mjs";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {
-    await users.connect()
-    console.log(`Server listening on port ${PORT}...`);
+  await users.connect();
+  console.log(`Server listening on port ${PORT}...`);
 });
-
-
 
 /**
  * Create a new user with the query parameters provided in the body
  */
-app.post('/users', asyncHandler(async (req, res) => {
-    const user = await users.createUser(req.body.name, 
-                            req.body.age, 
-                            req.body.email,
-                            req.body.phoneNumber);
+app.post(
+  "/users",
+  asyncHandler(async (req, res) => {
+    const user = await users.createUser(
+      req.body.name,
+      req.body.age,
+      req.body.email,
+      req.body.phoneNumber
+    );
     res.status(201).json(user);
-}));
+  })
+);
+
+/**
+ * Read using GET /users
+ */
+app.get(
+  "/users",
+  asyncHandler(async (req, res) => {
+    //build filter from req query
+    let filter = {};
+    if (req.query.name) {
+      filter.name = req.query.name;
+    }
+    if (req.query.age) {
+      filter.age = req.query.age;
+    }
+    if (req.query.email) {
+      filter.email = req.query.email;
+    }
+    if (req.query.phoneNumber) {
+      filter.phoneNumber = req.query.phoneNumber;
+    }
+
+    //async call model to search db
+    let result = await users.getUsers(filter);
+
+    res.status(200).json(result);
+  })
+);
+
+/**
+ * Read one using GET /users/:id
+ */
+app.get(
+  "/users/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    if (id) {
+      //call model to find it
+      let result = await users.getUserbyID(id);
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ Error: "Not Found" });
+      }
+    } else {
+      res.status(404).json({ Error: "Not Found" });
+    }
+  })
+);
+
+/**
+ * Update using PUT /users/:id
+ */
+app.put(
+  "/users/:id",
+  asyncHandler(async (req, res) => {
+    //build filter from req query
+    let filter = {};
+    if (req.body.name) {
+      filter.name = req.body.name;
+    }
+    if (req.body.age) {
+      filter.age = req.body.age;
+    }
+    if (req.body.email) {
+      filter.email = req.body.email;
+    }
+    if (req.body.phoneNumber) {
+      filter.phoneNumber = req.body.phoneNumber;
+    }
+
+    const id = req.params.id;
+
+    //async call model to search db
+    let result = await users.updateUser(id, filter);
+
+    if (result.matchedCount > 0) {
+      //call model to find it
+      let result = await users.getUserbyID(id);
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ Error: "Not Found" });
+      }
+    } else {
+      res.status(404).json({ Error: "Not Found" });
+    }
+  })
+);
--- assignment7/users_model.mjs
+++ assignment7/users_model.mjs
@@ -1,32 +1,33 @@
 // Get the mongoose object
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from "mongoose";
+import "dotenv/config";
 
-
-const USER_CLASS = 'User';
+const USER_CLASS = "User";
 
 let connection = undefined;
 
 /**
  * This function connects to the MongoDB server.
  */
-async function connect(){
-    try{
-        await mongoose.connect(process.env.MONGODB_CONNECT_STRING);
-        connection = mongoose.connection;
-        console.log("Successfully connected to MongoDB using Mongoose!");
-    } catch(err){
-        console.log(err);
-        throw Error(`Could not connect to MongoDB ${err.message}`)
-    }
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECT_STRING);
+    connection = mongoose.connection;
+    console.log("Successfully connected to MongoDB using Mongoose!");
+  } catch (err) {
+    console.log(err);
+    throw Error(`Could not connect to MongoDB ${err.message}`);
+  }
 }
-
 
 /**
  * Define the schema
  */
 const userSchema = mongoose.Schema({
-    // TODO: Define the schema to represent users
+  name: String,
+  age: Number,
+  email: String,
+  phoneNumber: Number,
 });
 
 /**
@@ -34,21 +35,51 @@ const userSchema = mongoose.Schema({
  */
 const User = mongoose.model(USER_CLASS, userSchema);
 
-
 /**
  * Create a user
  * @param {String} name
- * @param {Number} age 
+ * @param {Number} age
  * @param {String} email
  * @param {Number} phoneNumber
  * @returns A promise. Resolves to the JSON object for the document created by calling save
  */
 const createUser = async (name, age, email, phoneNumber) => {
-    // Call the constructor to create an instance of the model class User
-    const user = new User({ name: name, age: age, email: email, phoneNumber: phoneNumber });
-    // Call save to persist this object as a document in MongoDB
-    return user.save();
-}
+  // Call the constructor to create an instance of the model class User
+  const user = new User({
+    name: name,
+    age: age,
+    email: email,
+    phoneNumber: phoneNumber,
+  });
+  // Call save to persist this object as a document in MongoDB
+  return user.save();
+};
 
+/**
+ * Get user(s)
+ * @returns A promise. Resolves to an array of JSON objects that match the filter
+ */
+const getUsers = async (filter) => {
+  const query = User.find(filter);
+  return query.exec();
+};
 
-export { connect, createUser };
+/**
+ * Get user by id
+ * @returns A promise.
+ */
+const getUserbyID = async (id) => {
+  const query = User.findById(id);
+  return query.exec();
+};
+
+/**
+ * Update user by id (non - replacment)
+ * @returns A promise.
+ */
+const updateUser = async (id, body) => {
+  const query = await User.findById(id).updateOne(body);
+  return query;
+};
+
+export { connect, createUser, getUsers, getUserbyID, updateUser };
